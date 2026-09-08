fe53b4a87cb83dc31dce77942b63e2b749251a19
Amardeep Singh <amardeeps5201@gmail.com>
2026-09-07 20:09:09 -0400

    Add Privacy and Terms routes to App component

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Home from './pages/Home'
 import About from './pages/About'
 import HowItWorks from './pages/HowItWorks'
 import Contact from './pages/Contact'
+import Privacy from './pages/Privacy'
+import Terms from './pages/Terms'
 
 function App() {
   return (
@@ -15,6 +17,8 @@ function App() {
         <Route path="/how-it-works" element={<HowItWorks />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/privacy" element={<Privacy />} />
+        <Route path="/terms" element={<Terms />} />
       </Routes>
       <Footer />
     </BrowserRouter>
